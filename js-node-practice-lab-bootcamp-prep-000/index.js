e942cc85fa0e19c66c5b9cdcd2b475c63e8b401f
Vytautas Klimavicius <vyklimavicius@gmail.com>
2018-11-22 18:34:01 +0000

    Done.

---
+++ js-node-practice-lab-bootcamp-prep-000/index.js
@@ -0,0 +1,11 @@
+// write your code below!
+
+var name = "Vytautas"
+var height =71 
+
+console.log("Name:")
+console.log(name)
+
+console.log("Height:")
+console.log(height + 1)
+// console.log(parseInt(height) + 1)
